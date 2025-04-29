acaf4f0e34b8cadfbac39f3e9bc629f0ea7b4d8e
mcjannetp Mac Book <paul@mcjannetp-MacBook.local>
2025-04-29 11:19:23 -0400

    new logic for CRUD, fixed views for dynamic output, updated virtual env package deps

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room, Message
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -11,8 +11,10 @@ from .forms import RoomForm
 # ]
 
 def home(req):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = req.GET.get("q") if req.GET.get("q") != None else ""
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+    context = {"rooms": rooms, "topics": topics}
     return render(req, "base/home.html", context)
 
 def room(req, primary_key):
@@ -31,3 +33,23 @@ def createRoom(req):
 
     context = {"form": form}
     return render(req, "base/room_form.html", context)
+
+def updateRoom(req, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if req.method == "POST":
+        form = RoomForm(req.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        
+    context = {"form":form}
+    return render(req, "base/room_form.html", context)
+
+def deleteRoom(req, pk):
+    room = Room.objects.get(id=pk)
+    if req.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(req, "base/delete.html", {"obj": room})
